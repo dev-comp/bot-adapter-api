d88b0762a861187c3bd236f1973a63aa0b00050d
d.soldatkin <d.soldatkin@bftcom.com>
2016-09-19 22:31:06 +0300

    -

--- src/main/java/com/bftcom/devcomp/api/IBotManager.java
+++ src/main/java/com/bftcom/devcomp/api/IBotManager.java
@@ -9,10 +9,9 @@ import java.util.Map;
 public interface IBotManager {
   /**
    * @param id     Id of a new bot instance for fast access
-   * @param config any config properties necessary to start a bot instance
    * @return true if a bot session started without errors
    */
-  boolean startBotSession(String id, Map<String, String> config);
+  boolean startBotSession(String id, Map<String, String> userProps, Map<String, String> serviceProp);
 
   /**
    * @param id Id of a new bot instance for fast access
