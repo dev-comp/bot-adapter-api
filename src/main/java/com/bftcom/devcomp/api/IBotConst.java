d7bb9501226abc826decb09c74db6ecd07d88ab0
d.soldatkin <d.soldatkin@bftcom.com>
2016-09-23 17:45:26 +0300

    Меняем механизм работы с одной очереди на сервисе для всех ботов на такой, где для каждого бота на сервисе будет своя очередь

--- src/main/java/com/bftcom/devcomp/api/IBotConst.java
+++ src/main/java/com/bftcom/devcomp/api/IBotConst.java
@@ -5,16 +5,17 @@ package com.bftcom.devcomp.api;
  */
 public interface IBotConst {
   // ѕрефиксы очередей
-  String QUEUE_SERVICE_PREFIX = "SERVICE_QUEUE_";   // ќчереди, которые слушает сервис
-  String QUEUE_ADAPTER_PREFIX = "ADAPTER_QUEUE_";   // ќчереди, которые слушают адаптеры
-  String QUEUE_BOT_PREFIX = "BOT_QUEUE_";       // ќчереди, которые слушают экземпл€ры адаптеров
+  String QUEUE_SERVICE_PREFIX = "SERVICE_QUEUE_";     // ќчереди, которые слушает сервис
+  String QUEUE_ADAPTER_PREFIX = "ADAPTER_QUEUE_";     // ќчереди, которые слушают адаптеры
+  String QUEUE_TO_BOT_PREFIX = "TO_BOT_QUEUE_";       // ќчереди, которые слушают боты
+  String QUEUE_FROM_BOT_PREFIX = "FROM_BOT_QUEUE_";   // ќчереди, которые слушает сервис (дл€ каждого бота собственна€ очередь)
 
   // »мена системных пропертей, передаваемых в сообщении
   String PROP_ADAPTER_NAME = "ADAPTER_NAME";                // —войство, определ€ющие им€ адаптера
-  String PROP_BOT_NAME = "BOT_NAME";                    // —войство, определ€ющие им€ экземпл€ра адаптера
+  String PROP_BOT_NAME = "BOT_NAME";                        // —войство, определ€ющие им€ экземпл€ра адаптера
   String PROP_USER_NAME = "USER_NAME";                      // —войство, определ€ющие им€ пользовател€
 
   // »мена пользователских пропертей, передаваемых в сообщении
-  String PROP_BODY_TEXT = "BODY_TEXT";                // —войство, определ€ющие текст, передаваемый от бота и обратно (текст сообщени€)
+  String PROP_BODY_TEXT = "BODY_TEXT";                     // —войство, определ€ющие текст, передаваемый от бота и обратно (текст сообщени€)
 
 }
